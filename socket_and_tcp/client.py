9edde84cdfd1363f71da84f16a93b827ae00ac76
vicky89833 <vicky.jnv898@gmail.com>
2025-04-18 14:52:11 +0530

    tested socket and tcp connection

--- socket_and_tcp/client.py
+++ socket_and_tcp/client.py
@@ -1,11 +1,29 @@
-# Client
-def send_file(filename, host='localhost', port=5000):
+import socket
+import os
+
+def send_file(file_path, host='localhost', port=5000):
+    filename = os.path.basename(file_path)
+    filename_bytes = filename.encode()
+    filename_len = len(filename_bytes)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        with open(filename, 'rb') as f:
+
+        # Step 1: Send filename length
+        s.sendall(filename_len.to_bytes(4, 'big'))
+
+        # Step 2: Send filename
+        s.sendall(filename_bytes)
+
+        # Step 3: Send file data
+        with open(file_path, 'rb') as f:
             while True:
                 data = f.read(1024)
                 if not data:
                     break
                 s.sendall(data)
-        print("File sent successfully")
+
+        print(f"File '{filename}' sent successfully!")
+
+file_path = "/home/vicky/Downloads/B21BB033_LAB_3.pdf"
+send_file(file_path)
--- socket_and_tcp/server.py
+++ socket_and_tcp/server.py
@@ -1,6 +1,6 @@
 # Server
 import socket
-
+import os
 def start_server(host='0.0.0.0', port=5000):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
@@ -10,10 +10,21 @@ def start_server(host='0.0.0.0', port=5000):
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
-            with open('received_file', 'wb') as f:
+
+            # Step 1: Receive filename length (fixed 4 bytes, big-endian integer)
+            filename_len_bytes = conn.recv(4)
+            filename_len = int.from_bytes(filename_len_bytes, 'big')
+
+            # Step 2: Receive filename
+            filename = conn.recv(filename_len).decode()
+            print(f"Receiving file: {filename}")
+
+
+            with open(filename, 'wb') as f:
                 while True:
                     data = conn.recv(1024)
                     if not data:
                         break
                     f.write(data)
-            print("File received successfully")
+            print(f"File {filename} received successfully.")
+start_server()
